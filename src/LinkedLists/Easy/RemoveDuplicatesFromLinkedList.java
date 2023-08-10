3b96612c30857c0d792eb60a94682caae4394600
Joseph Calver <josephcalver@gmail.com>
2023-08-10 15:01:12 +0100

    Remove Duplicates from LinkedList

---
+++ src/LinkedLists/Easy/RemoveDuplicatesFromLinkedList.java
@@ -0,0 +1,73 @@
+package LinkedLists.Easy;
+
+public class RemoveDuplicatesFromLinkedList {
+
+    public static void main(String[] args) {
+
+        Node n9 = new Node(5, null);
+        Node n8 = new Node(5, n9);
+        Node n7 = new Node(4, n8);
+        Node n6 = new Node(4, n7);
+        Node n5 = new Node(4, n6);
+        Node n4 = new Node(3, n5);
+        Node n3 = new Node(2, n4);
+        Node n2 = new Node(1, n3);
+        Node n1 = new Node(1, n2);
+
+        printLinkedList(n1);
+
+        Node head = removeDups(n1);
+
+        printLinkedList(head);
+
+    }
+
+    public static Node removeDups(Node head) {
+
+        Node current = head, nextDiff = null;
+
+        while (current != null) {
+
+            nextDiff = current.next;
+
+            while (nextDiff != null && current.value == nextDiff.value) {
+
+                nextDiff = nextDiff.next;
+
+            }
+
+            current.next = nextDiff;
+            current = current.next;
+
+        }
+
+        return head;
+
+    }
+
+    public static void printLinkedList(Node node) {
+
+        while (node != null) {
+
+            System.out.print(node.value + " --> ");
+            node = node.next;
+
+        }
+
+        System.out.println();
+
+    }
+
+    public static class Node {
+
+        int value;
+        Node next;
+
+        public Node(int value, Node next) {
+            this.value = value;
+            this.next = next;
+        }
+
+    }
+
+}
