20df9990a66f3db41b8957eb634bfd554bfa529b
Joseph Calver <josephcalver@gmail.com>
2023-08-14 13:53:11 +0100

    Palindrome Check

--- src/Strings/Easy/PalindromeCheck.java
+++ src/Strings/Easy/PalindromeCheck.java
@@ -1,2 +1,28 @@
-package Strings.Easy;public class PalindromeCheck {
+package Strings.Easy;
+
+public class PalindromeCheck {
+
+    public static void main(String[] args) {
+
+        String s = "abcdedcba";
+
+        System.out.println(isPalindrome(s));
+
+    }
+
+//    O(n) time | O(1) space
+
+    public static boolean isPalindrome(String s) {
+
+        int left = 0, right = s.length() - 1;
+
+        while (left < right) {
+
+            if (s.charAt(left++) != s.charAt(right--)) return false;
+        }
+
+        return true;
+
+    }
+
 }
