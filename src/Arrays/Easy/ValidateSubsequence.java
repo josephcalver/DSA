18f2f396dbeaf1a74d69e74423c5eae12a0d7bf6
Joseph Calver <josephcalver@gmail.com>
2023-08-14 13:54:46 +0100

    Validate Subsequence

--- src/Arrays/Easy/ValidateSubsequence.java
+++ src/Arrays/Easy/ValidateSubsequence.java
@@ -1,2 +1,34 @@
-package Arrays.Easy;public class ValidateSubsequence {
+package Arrays.Easy;
+
+public class ValidateSubsequence {
+
+    public static void main(String[] args) {
+
+        int[] array = {1, 2, 3, 4, 5};
+        int[] validSubsequence = {1, 4, 5};
+        int[] invalidSubsequence = {1, 5, 4};
+
+        System.out.println(isValidSubsequence(array, validSubsequence));
+        System.out.println(isValidSubsequence(array, invalidSubsequence));
+
+    }
+
+//    O(n) time | O(1) space
+
+    public static boolean isValidSubsequence(int[] array, int[] subsequence) {
+
+        int i = 0, j = 0;
+
+        while (i < subsequence.length && j < array.length) {
+
+            if (subsequence[i] == array[j]) i++;
+
+            j++;
+
+        }
+
+        return i == subsequence.length;
+
+    }
+
 }
