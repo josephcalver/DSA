d4717252f42d641abcb6b6ba3b25d29ca4b78390
Joseph Calver <josephcalver@gmail.com>
2023-10-19 14:31:32 +0100

    BST Construction (Iterative)

--- src/BinarySearchTrees/Medium/BSTConstruction.java
+++ src/BinarySearchTrees/Medium/BSTConstruction.java
@@ -44,9 +44,61 @@ public class BSTConstruction {
 
         System.out.println(root.contains(22));
 
+        BSTIterative root2 = new BSTIterative(10);
+
+        root2.insert(5);
+        root2.insert(15);
+        root2.insert(2);
+        root2.insert(5);
+        root2.insert(13);
+        root2.insert(22);
+        root2.insert(1);
+        root2.insert(14);
+
+        preOrderTraversal(root2);
+        System.out.println();
+
+        root2.remove(5);
+        preOrderTraversal(root2);
+        System.out.println();
+
+        root2.remove(5);
+        preOrderTraversal(root2);
+        System.out.println();
+
+        root2.remove(1);
+        preOrderTraversal(root2);
+        System.out.println();
+
+        root2.remove(10);
+        preOrderTraversal(root2);
+        System.out.println();
+
+        root2.insert(9);
+        preOrderTraversal(root2);
+        System.out.println();
+
+        root2.insert(13);
+        preOrderTraversal(root2);
+        System.out.println();
+
+        System.out.println(root2.contains(22));
+
     }
 
     public static void preOrderTraversal(BSTRecursive node) {
+
+        if (node == null) return;
+
+        System.out.println(node.value + " --> ");
+
+        preOrderTraversal(node.left);
+
+        preOrderTraversal(node.right);
+
+    }
+
+    public static void preOrderTraversal(BSTIterative node) {
 
         if (node == null) return;
 
---
+++ src/BinarySearchTrees/Medium/BSTIterative.java
@@ -0,0 +1,173 @@
+package BinarySearchTrees.Medium;
+
+public class BSTIterative {
+
+    public int value;
+    public BSTIterative left;
+    public BSTIterative right;
+
+    public BSTIterative(int value) {
+        this.value = value;
+    }
+
+    public BSTIterative insert(int value) {
+
+        // Write your code here.
+        // Do not edit the return statement of this method.
+
+        BSTIterative current = this;
+
+        while (true) {
+
+            if (value < current.value) {
+
+                if (current.left != null) {
+
+                    current = current.left;
+
+                } else {
+
+                    current.left = new BSTIterative(value);
+
+                    break;
+
+                }
+
+            } else {
+
+                if (current.right != null) {
+
+                    current = current.right;
+
+                } else {
+
+                    current.right = new BSTIterative(value);
+
+                    break;
+
+                }
+
+
+            }
+
+
+        }
+
+        return this;
+
+    }
+
+    public boolean contains(int value) {
+
+        BSTIterative current = this;
+
+        while (current != null) {
+
+            if (value < current.value) {
+
+                current = current.left;
+
+            } else if (value > current.value) {
+
+                current = current.right;
+
+            } else {
+
+                return true;
+
+            }
+
+        }
+
+        return false;
+
+    }
+
+    public BSTIterative remove(int value) {
+
+        remove(value, null);
+
+        return this;
+
+    }
+
+    public void remove(int value, BSTIterative parent) {
+
+        // Write your code here.
+        // Do not edit the return statement of this method.
+
+        BSTIterative current = this;
+
+        while (current != null) {
+
+            if (value < current.value) {
+
+                parent = current;
+                current = current.left;
+
+            } else if (value > current.value) {
+
+                parent = current;
+                current = current.right;
+
+            } else {
+
+                if (current.left != null && current.right != null) { // 2 child approach applies to any node, including root
+
+                    int successor = current.right.getSuccessor();
+
+                    current.value = successor;
+
+                    current.right.remove(successor, current);
+
+                } else if (parent == null) { // single child approach different for root node
+
+                    if (current.left != null) {
+
+                        current.value = left.value;
+                        current.right = current.left.right;
+                        current.left = current.left.left;
+
+                    } else if (current.right != null) {
+
+                        current.value = right.value;
+                        current.left = current.right.left;
+                        current.right = current.right.right;
+
+                    } else {
+
+                        // this is a single-node tree; do nothing
+
+                    }
+
+                } else if (parent.left == current) {
+
+                    parent.left = current.left != null ? current.left : current.right;
+
+                } else if (parent.right == current) {
+
+                    parent.right = current.left != null ? current.left : current.right;
+
+                }
+
+                break;
+
+            }
+
+        }
+
+    }
+
+    public int getSuccessor() {
+
+        if (this.left == null) {
+
+            return this.value;
+
+        }
+
+        return this.left.getSuccessor();
+
+    }
+
+}
